4d0d2243bad2e2e439f3d7a1c53c2ed91c4fd3ef
ArunaMaurya221B <aruna.maurya12@gmail.com>
2018-07-12 18:57:08 +0530

    Added the test data and changed the splitting of data

--- main.py
+++ main.py
@@ -4,23 +4,28 @@ import pandas as pd
 import numpy as nu
 import matplotlib.pyplot as pt
 from sklearn import tree
+from sklearn.metrics import accuracy_score
 
-data = pd.read_csv("/home/icts/digit_recognizer/train.csv").as_matrix()
-#print data
+
+data_train = pd.read_csv("/home/icts/digit_recognizer/train.csv").as_matrix()
+data_test = pd.read_csv("/home/icts/digit_recognizer/test.csv").as_matrix()
+print data_test
 
 #create training and testing data
 
-feature_train = data[0:2100,1:]
-feature_test = data[2100:,1:]
-label_train = data[0:2100,0]
-label_test = data[2100:,0]
+feature_train = data_train[0:42000,1:]
+feature_test = data_test[0:2800:,0:]
+label_train = data_train[0:42000,0]
 
 #creating classifier and predicting
 
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(feature_train, label_train)
-x = feature_test[8]
-print (clf.predict([feature_test[8]]))
+y_pred = clf.predict(feature_test)
+print y_pred
+#print (accuracy_score(feature_test, y_pred))
+print (clf.predict([feature_test[18]]))
+x = feature_test[18]
 x.shape=(28,28)
 pt.imshow(255-x, cmap='gray')
 pt.show()
